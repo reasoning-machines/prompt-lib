fd232d6ed802691a23f5b84ca764fe490ad16fc8
Aman Madaan <amadaan@cs.cmu.edu>
2022-09-21 16:20:59 -0400

    [gsm] add new gsm tasks

--- prompts/task_id_to_prompt.py
+++ prompts/task_id_to_prompt.py
@@ -1,8 +1,10 @@
 from prompts.gsm import gsm_task_id_to_prompt
 from prompts.sports import sports_task_id_to_prompt
 from prompts.date import date_task_id_to_prompt
+from prompts.sorting import sorting_task_id_to_prompt
 
 task_id_to_prompt = dict()
 task_id_to_prompt.update(gsm_task_id_to_prompt)
 task_id_to_prompt.update(sports_task_id_to_prompt)
 task_id_to_prompt.update(date_task_id_to_prompt)
+task_id_to_prompt.update(sorting_task_id_to_prompt)
