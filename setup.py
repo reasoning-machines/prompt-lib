0d61bf9e7fac2643d5df0bde755820c672c4a617
Aman Madaan <amadaan@cs.cmu.edu>
2022-11-27 18:46:26 -0500

    [meta] add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+
+from setuptools import setup, find_packages
+
+setup(name='prompt-lib',
+      version='0.1',
+      description="Library for running few-shot inference on large language models",
+      packages=find_packages(),
+      install_requires=['openai==0.23.0', 'pandas==1.4.4', 'requests==2.28.1', 'scikit_learn==1.1.2', 'statsmodels==0.13.2', 'tqdm==4.49.0', 'wandb==0.13.2'],
+      license='MIT',
+      long_description=open('README.md').read(),
+)
