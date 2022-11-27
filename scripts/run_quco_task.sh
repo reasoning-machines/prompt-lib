3dacc6a7f7482af91bd96b81e9a230cda3cfed12
Aman Madaan <amadaan@cs.cmu.edu>
2022-11-27 13:09:55 -0500

    [feat] properly pass newlines as arguments

--- scripts/run_quco_task.sh
+++ scripts/run_quco_task.sh
@@ -9,37 +9,43 @@ if [ "${MODEL_NAME}" == "text-davinci-002" ]; then
     REQ_PER_MIN=1600000
 elif [ "${MODEL_NAME}" == "code-davinci-002" ]; then
     REQ_PER_MIN=16  # code-davinci-002 has a much lower throughput
+elif [ "${MODEL_NAME}" == "code-davinci-001" ]; then
+    REQ_PER_MIN=16  # code-davinci-001 has a much lower throughput
+elif [ "${MODEL_NAME}" == "code-cushman-001" ]; then
+    REQ_PER_MIN=16  # code-cushman-001 has a much lower throughput
 else
     echo "Invalid model name"
     exit 1
 fi
-
 NUM_QUESTIONS_PER_THREAD=200
 MAX_TOKENS=600
 
 QUESTION_PREFIX="# Q: "
 ANSWER_PREFIX="# solution using Python:\n"
 FINAL_ANSWER_PREFIX=""
-INTRA_EXAMPLE_SEP="\n\n"
-INTER_EXAMPLE_SEP="\n\n\n"
+INTRA_EXAMPLE_SEP="\n"
+INTER_EXAMPLE_SEP="\n\n"
 
 EVAL_FUNC="get_acc_from_python_thoughts"
 
 TEMP=0.0
 NUM_EXAMPLES=-1
 
+# declare -a ALL_TASKS=("gsm_quco" "gsmhard_quco" "svamp_quco" "mawpsaddsub_quco" "mawpsmultiarith_quco" "mawpssingleeq_quco" "mawpssingleop_quco" "asdiv_quco")
+
 declare -a ALL_TASKS=("gsm_quco" "gsmhard_quco" "svamp_quco" "mawpsaddsub_quco" "mawpsmultiarith_quco" "mawpssingleeq_quco" "mawpssingleop_quco" "asdiv_quco")
 
-declare -a ALL_TASKS=("fiveexamples_quco")
+declare -a ALL_TASKS=("$@")
+
+
+
 
 declare -a SEEDS=(0)
 
 # declare -a ALL_TASKS=("humaneval_stream")
 
-for TASK in "${ALL_TASKS[@]}"; do
-
-    for SEED in "${SEEDS[@]}"; do
-
+for SEED in "${SEEDS[@]}"; do
+    for TASK in "${ALL_TASKS[@]}"; do
         echo "Running $TASK with seed $SEED"
         ARGS=(--task_id ${TASK}
             --num_examples ${NUM_EXAMPLES} # -1 means use all examples in the prompt
@@ -50,11 +56,11 @@ for TASK in "${ALL_TASKS[@]}"; do
             --seed ${SEED}   # decides order of examples in the prompt
             --num_questions_per_thread ${NUM_QUESTIONS_PER_THREAD}  # number of questions to ask per thread
             --max_tokens ${MAX_TOKENS}
-            --question_prefix "${QUESTION_PREFIX}"
-            --answer_prefix "${ANSWER_PREFIX}"
-            --final_answer_prefix "${FINAL_ANSWER_PREFIX}"
-            --intra_example_sep "${INTRA_EXAMPLE_SEP}"
-            --inter_example_sep "${INTER_EXAMPLE_SEP}"
+            --question_prefix  $\'"${QUESTION_PREFIX}"\'
+            --answer_prefix $\'"${ANSWER_PREFIX}"\'
+            --final_answer_prefix $\'"${FINAL_ANSWER_PREFIX}"\'
+            --intra_example_sep $\'"${INTRA_EXAMPLE_SEP}"\'
+            --inter_example_sep $\'"${INTER_EXAMPLE_SEP}"\'
             --wandb_project "pal"
             --eval_function "${EVAL_FUNC}"
             --temperature ${TEMP}
@@ -62,7 +68,9 @@ for TASK in "${ALL_TASKS[@]}"; do
 
 
         echo "python3 run.py ${ARGS[@]}"
-        python -u prompt-lib/query_openai.py "${ARGS[@]}"
+        python -u prompt-lib/query_openai.py "${ARGS[@]}" > logs/"${TASK}_${MODEL_NAME}_s${SEED}.log" 2>&1
+        # python -u prompt-lib/query_openai.py "${ARGS[@]}"
+
     done
 done
 
