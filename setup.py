e9165843cf4b734ee8e5a049eb0e85298f9b0c6d
Aman Madaan <amadaan@cs.cmu.edu>
2023-04-02 22:45:20 -0400

    Update setup for chatgpt api

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(name='prompt-lib',
       version='0.1',
       description="Library for running few-shot inference on large language models",
       packages=find_packages(),
-      install_requires=['openai==0.23.0', 'pandas', 'tqdm==4.49.0', 'wandb==0.13.2'],
+      install_requires=['openai==0.27.2', 'pandas', 'tqdm==4.49.0', 'wandb==0.13.2'],
       license='MIT',
       long_description=open('README.md').read(),
 )
