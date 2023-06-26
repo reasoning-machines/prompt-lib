e1ba0a5837dba727b0ded5a1b0cf75d79301d5ef
Aman Madaan <amadaan@cs.cmu.edu>
2023-06-25 22:45:43 -0400

    [feat] add openai.error.ServiceUnavailableError to retry_with_exponential_backoff

--- prompt_lib/backends/openai_api.py
+++ prompt_lib/backends/openai_api.py
@@ -24,7 +24,7 @@ def retry_with_exponential_backoff(
     exponential_base: float = 2,
     jitter: bool = True,
     max_retries: int = 10,
-    errors: tuple = (openai.error.RateLimitError,),
+    errors: tuple = (openai.error.RateLimitError,openai.error.ServiceUnavailableError,),
 ):
     """Retry a function with exponential backoff."""
 
